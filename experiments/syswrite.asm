7c3eb5c6693ad9f248b0b9d1759645382d9e0cc8
nordic16 <diogo6204@gmail.com>
2024-10-31 09:23:36 +0000

    added sys_write example

---
+++ experiments/syswrite.asm
@@ -0,0 +1,23 @@
+%macro sys_write 2 ; basically a function with two arguments
+    mov eax, 1 ; sys_write
+    mov edi, 1 ; stdout
+    mov esi, %1 ; value to write (first arg)
+    mov edx, %2 ; len (second arg)
+    syscall
+%endmacro
+
+section .rodata
+    msg: db "Hello world",10
+    len: equ $-msg
+
+section .text
+global main
+main:
+    mov rbp, rsp; for correct debugging
+    xor rax, rax
+    
+    ; calls sys_write macro with msg and len as args.
+    ; quick note: in order to properly perform a sys_write, one must transfer not the value to esi but rather its address.
+    sys_write msg, len
+    
+    ret
